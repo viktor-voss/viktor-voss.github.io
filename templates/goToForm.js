820b19386d571b68b3f18d6b20eb23fbb12f3049
Viktor Voss <me@viteke.com>
2025-09-27 00:26:17 +0200

    initial load

---
+++ templates/goToForm.js
@@ -0,0 +1,1 @@
+function goToForm(name) { navigator.clipboard.writeText(name); window.open("https://docs.google.com/forms/d/e/1FAIpQLSeD1v34aZwA9VpG4ZL8RwQfV6xec9JgiKz0tM8TYnrDo7Jq7w/viewform","bestellen_form"); }
